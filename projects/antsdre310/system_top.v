4153e5c367a310fb6bed5b4b068c70c26b0d1ba7
black_pigeon <1530604142@qq.com>
2022-11-06 19:11:18 +0800

    antsdr_e310 hdl project compatible to adrv9361

--- projects/antsdre310/system_top.v
+++ projects/antsdre310/system_top.v
@@ -36,7 +36,6 @@
 `timescale 1ns/100ps
 
 module system_top (
-
   inout   [14:0]  ddr_addr,
   inout   [ 2:0]  ddr_ba,
   inout           ddr_cas_n,
@@ -44,10 +43,10 @@ module system_top (
   inout           ddr_ck_p,
   inout           ddr_cke,
   inout           ddr_cs_n,
-  inout   [ 1:0]  ddr_dm,
+  inout   [ 3:0]  ddr_dm,
   inout   [31:0]  ddr_dq,
-  inout   [ 1:0]  ddr_dqs_n,
-  inout   [ 1:0]  ddr_dqs_p,
+  inout   [ 3:0]  ddr_dqs_n,
+  inout   [ 3:0]  ddr_dqs_p,
   inout           ddr_odt,
   inout           ddr_ras_n,
   inout           ddr_reset_n,
@@ -55,7 +54,7 @@ module system_top (
 
   inout           fixed_io_ddr_vrn,
   inout           fixed_io_ddr_vrp,
-  inout   [31:0]  fixed_io_mio,
+  inout   [53:0]  fixed_io_mio,
   inout           fixed_io_ps_clk,
   inout           fixed_io_ps_porb,
   inout           fixed_io_ps_srstb,
@@ -63,18 +62,24 @@ module system_top (
   inout           iic_scl,
   inout           iic_sda,
 
-  input           rx_clk_in,
-  input           rx_frame_in,
-  input   [11:0]  rx_data_in,
-  output          tx_clk_out,
-  output          tx_frame_out,
-  output  [11:0]  tx_data_out,
+  input           rx_clk_in_p,
+  input           rx_clk_in_n,
+  input           rx_frame_in_p,
+  input           rx_frame_in_n,
+  input   [ 5:0]  rx_data_in_p,
+  input   [ 5:0]  rx_data_in_n,
+  output          tx_clk_out_p,
+  output          tx_clk_out_n,
+  output          tx_frame_out_p,
+  output          tx_frame_out_n,
+  output  [ 5:0]  tx_data_out_p,
+  output  [ 5:0]  tx_data_out_n,
 
   output          enable,
   output          txnrx,
-  input           clk_out,
 
   inout           gpio_resetb,
+  inout           gpio_sync,
   inout           gpio_en_agc,
   inout   [ 3:0]  gpio_ctl,
   inout   [ 7:0]  gpio_status,
@@ -83,50 +88,54 @@ module system_top (
   output          spi_clk,
   output          spi_mosi,
   input           spi_miso,
-  output          VCRX1_H,
-  output          VCRX1_L,
-  output          VCTX1_H,
-  output          VCTX1_L,
-  output          VCRX2_H,
-  output          VCRX2_L,
-  output          VCTX2_H,
-  output          VCTX2_L,
-  
-  // output          ltc2630_mosi_0,
-  // output          ltc2630_ncs_0,
-  // output          ltc2630_sclk_0,
 
-  output          pl_spi_clk_o,
-  output          pl_spi_mosi,
-  input           pl_spi_miso);
+  output          rx1_band_sel_h,
+  output          rx1_band_sel_l,
+  output          tx1_band_sel_h,
+  output          tx1_band_sel_l,
+  output          rx2_band_sel_h,
+  output          rx2_band_sel_l,
+  output          tx2_band_sel_h,
+  output          tx2_band_sel_l
+  );
 
   // internal signals
+  wire    [95:0]  gp_out_s;
+  wire    [95:0]  gp_in_s;
+  wire    [63:0]  gpio_i;
+  wire    [63:0]  gpio_o;
+  wire    [63:0]  gpio_t;
+  wire    [85:0]  gp_out;
+  wire   [85:0]  gp_in;
+  // assignments
+  assign gp_out[85:0] = gp_out_s[85:0];
+  assign gp_in_s[95:86] = gp_out_s[95:86];
+  assign gp_in_s[85: 0] = gp_in[85:0];
 
-  wire    [24:0]  gpio_i;
-  wire    [24:0]  gpio_o;
-  wire    [24:0]  gpio_t;
+  assign gpio_i[31:0] = gpio_o[31:0];
+  assign gpio_i[63:47] = gpio_o[63:47];
+
+
+  assign rx1_band_sel_h = gpio_o[0];
+  assign rx1_band_sel_l = gpio_o[1];
+  assign tx1_band_sel_h = gpio_o[2];
+  assign tx1_band_sel_l = gpio_o[3];
+  assign rx2_band_sel_h = gpio_o[4];
+  assign rx2_band_sel_l = gpio_o[5];
+  assign tx2_band_sel_h = gpio_o[6];
+  assign tx2_band_sel_l = gpio_o[7];
+
+  ad_iobuf #(.DATA_WIDTH(15)) i_iobuf (
+    .dio_t ( gpio_t[46:32]),
+    .dio_i ( gpio_o[46:32]),
+    .dio_o ( gpio_i[46:32]),
+    .dio_p ({ gpio_resetb,        // 46:46
+              gpio_sync,          // 45:45
+              gpio_en_agc,        // 44:44
+              gpio_ctl,           // 43:40
+              gpio_status}));     // 39:32
 
   // instantiations
-
-  ad_iobuf #(.DATA_WIDTH(14)) i_iobuf (
-    .dio_t (gpio_t[13:0]),
-    .dio_i (gpio_o[13:0]),
-    .dio_o (gpio_i[13:0]),
-    .dio_p ({ gpio_resetb,        // 13:13
-              gpio_en_agc,        // 12:12
-              gpio_ctl,           // 11: 8
-              gpio_status}));     //  7: 0
-
-  assign gpio_i[24:14] = gpio_o[24:14];
-  assign VCRX1_H=gpio_o[17];
-  assign VCRX1_L=gpio_o[18];
-  assign VCTX1_H=gpio_o[19];
-  assign VCTX1_L=gpio_o[20];
-  assign VCRX2_H=gpio_o[21];
-  assign VCRX2_L=gpio_o[22];
-  assign VCTX2_H=gpio_o[23];
-  assign VCTX2_L=gpio_o[24];
-  
 
   system_wrapper i_system_wrapper (
     .ddr_addr (ddr_addr),
@@ -151,19 +160,27 @@ module system_top (
     .fixed_io_ps_clk (fixed_io_ps_clk),
     .fixed_io_ps_porb (fixed_io_ps_porb),
     .fixed_io_ps_srstb (fixed_io_ps_srstb),
+    .gp_in_0 (gp_in_s[31:0]),
+    .gp_in_1 (gp_in_s[63:32]),
+    .gp_in_2 (gp_in_s[95:64]),
+    .gp_in_3 (32'd0),
+    .gp_out_0 (gp_out_s[31:0]),
+    .gp_out_1 (gp_out_s[63:32]),
+    .gp_out_2 (gp_out_s[95:64]),
+    .gp_out_3 (),
     .gpio_i (gpio_i),
     .gpio_o (gpio_o),
     .gpio_t (gpio_t),
+    .gps_pps (1'b0),
     .iic_main_scl_io (iic_scl),
     .iic_main_sda_io (iic_sda),
-    .rx_clk_in (rx_clk_in),
-    .rx_data_in (rx_data_in),
-    .rx_frame_in (rx_frame_in),
-    
-    // .ltc2630_mosi_0(ltc2630_mosi_0),
-    // .ltc2630_ncs_0(ltc2630_ncs_0),
-    // .ltc2630_sclk_0(ltc2630_sclk_0),
-
+    .otg_vbusoc (1'b0),
+    .rx_clk_in_n (rx_clk_in_n),
+    .rx_clk_in_p (rx_clk_in_p),
+    .rx_data_in_n (rx_data_in_n),
+    .rx_data_in_p (rx_data_in_p),
+    .rx_frame_in_n (rx_frame_in_n),
+    .rx_frame_in_p (rx_frame_in_p),
     .spi0_clk_i (1'b0),
     .spi0_clk_o (spi_clk),
     .spi0_csn_0_o (spi_csn),
@@ -173,24 +190,29 @@ module system_top (
     .spi0_sdi_i (spi_miso),
     .spi0_sdo_i (1'b0),
     .spi0_sdo_o (spi_mosi),
-
-    .spi_clk_i(1'b0),
-    .spi_clk_o(pl_spi_clk_o),
-    .spi_csn_i(1'b1),
-    .spi_csn_o(),
-    .spi_sdi_i(pl_spi_miso),
-    .spi_sdo_i(1'b0),
-    .spi_sdo_o(pl_spi_mosi),
-
-    .tx_clk_out (tx_clk_out),
-    .tx_data_out (tx_data_out),
-    .tx_frame_out (tx_frame_out),
+    .spi1_clk_i (1'b0),
+    .spi1_clk_o (),
+    .spi1_csn_0_o (),
+    .spi1_csn_1_o (),
+    .spi1_csn_2_o (),
+    .spi1_csn_i (1'b1),
+    .spi1_sdi_i (1'b0),
+    .spi1_sdo_i (1'b0),
+    .spi1_sdo_o (),
+    .tdd_sync_i (1'b0),
+    .tdd_sync_o (),
+    .tdd_sync_t (),
+    .tx_clk_out_n (tx_clk_out_n),
+    .tx_clk_out_p (tx_clk_out_p),
+    .tx_data_out_n (tx_data_out_n),
+    .tx_data_out_p (tx_data_out_p),
+    .tx_frame_out_n (tx_frame_out_n),
+    .tx_frame_out_p (tx_frame_out_p),
     .txnrx (txnrx),
-    .up_enable (gpio_o[15]),
-    .up_txnrx (gpio_o[16]));
+    .up_enable (gpio_o[47]),
+    .up_txnrx (gpio_o[48]));
 
 endmodule
 
 // ***************************************************************************
 // ***************************************************************************
-
