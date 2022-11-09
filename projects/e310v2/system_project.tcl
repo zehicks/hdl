0a4e896f2d275038f5061bee798a4e011ad021ae
black-pigeon <1530604142@qq.com>
2022-11-09 14:50:29 +0800

    add a debug version of e310v2 hdl project

---
+++ projects/e310v2/system_project.tcl
@@ -0,0 +1,19 @@
+
+source ../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+set p_device "xc7z020clg400-2"
+adi_project e310v2
+adi_project_files e310v2 [list \
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "system.xdc" \
+  "b205_ref_pll.v" \
+  "ad5662_auto_spi.v" \
+  "./ip/gen_clks/gen_clks.xci" \
+  "./ip/vio_0/vio_0.xci" \
+  "system_top.v" ]
+
+adi_project_run e310v2
+source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
---
+++ projects/e310v2/system_top.v
@@ -0,0 +1,308 @@
+// ***************************************************************************
+// ***************************************************************************
+// Copyright 2014 - 2017 (c) Analog Devices, Inc. All rights reserved.
+//
+// In this HDL repository, there are many different and unique modules, consisting
+// of various HDL (Verilog or VHDL) components. The individual modules are
+// developed independently, and may be accompanied by separate and unique license
+// terms.
+//
+// The user should read each of these license terms, and understand the
+// freedoms and responsibilities that he or she has by using this source/core.
+//
+// This core is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.
+//
+// Redistribution and use of source or resulting binaries, with or without modification
+// of this file, are permitted under one of the following two license terms:
+//
+//   1. The GNU General Public License version 2 as published by the
+//      Free Software Foundation, which can be found in the top level directory
+//      of this repository (LICENSE_GPL2), and also online at:
+//      <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+//
+// OR
+//
+//   2. An ADI specific BSD license, which can be found in the top level directory
+//      of this repository (LICENSE_ADIBSD), and also on-line at:
+//      https://github.com/analogdevicesinc/hdl/blob/master/LICENSE_ADIBSD
+//      This will allow to generate bit files and not release the source code,
+//      as long as it attaches to an ADI device.
+//
+// ***************************************************************************
+// ***************************************************************************
+
+`timescale 1ns/100ps
+
+module system_top (
+  output          MDIO_PHY_mdc,
+  inout           MDIO_PHY_mdio_io,
+  input [3:0]     RGMII_rd,
+  input           RGMII_rx_ctl,
+  input           RGMII_rxc,
+  output [3:0]    RGMII_td,
+  output          RGMII_tx_ctl,
+  output          RGMII_txc,
+  output          eth_rst_n ,
+
+  inout   [14:0]  ddr_addr,
+  inout   [ 2:0]  ddr_ba,
+  inout           ddr_cas_n,
+  inout           ddr_ck_n,
+  inout           ddr_ck_p,
+  inout           ddr_cke,
+  inout           ddr_cs_n,
+  inout   [ 3:0]  ddr_dm,
+  inout   [31:0]  ddr_dq,
+  inout   [ 3:0]  ddr_dqs_n,
+  inout   [ 3:0]  ddr_dqs_p,
+  inout           ddr_odt,
+  inout           ddr_ras_n,
+  inout           ddr_reset_n,
+  inout           ddr_we_n,
+
+  inout           fixed_io_ddr_vrn,
+  inout           fixed_io_ddr_vrp,
+  inout   [53:0]  fixed_io_mio,
+  inout           fixed_io_ps_clk,
+  inout           fixed_io_ps_porb,
+  inout           fixed_io_ps_srstb,
+
+  input           rx_clk_in_p,
+  input           rx_clk_in_n,
+  input           rx_frame_in_p,
+  input           rx_frame_in_n,
+  input   [ 5:0]  rx_data_in_p,
+  input   [ 5:0]  rx_data_in_n,
+  output          tx_clk_out_p,
+  output          tx_clk_out_n,
+  output          tx_frame_out_p,
+  output          tx_frame_out_n,
+  output  [ 5:0]  tx_data_out_p,
+  output  [ 5:0]  tx_data_out_n,
+
+  output          enable,
+  output          txnrx,
+
+  inout           gpio_resetb,
+  inout           gpio_sync,
+  inout           gpio_en_agc,
+  inout   [ 3:0]  gpio_ctl,
+  inout   [ 7:0]  gpio_status,
+
+  output          spi_csn,
+  output          spi_clk,
+  output          spi_mosi,
+  input           spi_miso,
+
+  // clock form vctcxo
+  input  wire	 			      CLK_40MHz_FPGA  ,
+  // PPS or 10 MHz
+  input  wire             PPS_IN          ,
+  input  wire             CLKIN_10MHz     ,
+
+  // Clock disciplining / AD5660 controls
+  output wire             CLK_40M_DAC_nSYNC,
+  output wire             CLK_40M_DAC_SCLK ,
+  output wire             CLK_40M_DAC_DIN ,
+
+  output wire             FE_TXRX2_SEL2 ,
+  output wire             FE_TXRX2_SEL1 ,
+  output wire             FE_TXRX1_SEL2 ,
+  output wire             FE_TXRX1_SEL1 ,
+  output wire             FE_RX2_SEL2 ,
+  output wire             FE_RX2_SEL1 ,
+  output wire             FE_RX1_SEL2 ,
+  output wire             FE_RX1_SEL1 ,
+
+
+  output wire             GPS_RSTN,
+  output wire             GPS_PWEN,
+  input  wire             GPS_PPS,
+  output wire             GPS_EXT1,
+  input  wire             GPS_EXT0,
+
+  output wire             tx_amp_en1,
+  output wire             tx_amp_en2,
+
+  inout wire  [28:0]      GPIOB
+  );
+
+  // internal signals
+  wire    [95:0]  gp_out_s;
+  wire    [95:0]  gp_in_s;
+  wire    [63:0]  gpio_i;
+  wire    [63:0]  gpio_o;
+  wire    [63:0]  gpio_t;
+  wire    [85:0]  gp_out;
+  wire    [85:0]  gp_in;
+
+  wire            int_40mhz   ;
+  wire            ref_pll_clk ;
+  wire            locked      ;
+  wire            ref_sel;
+  wire            ext_ref;
+  wire            ext_ref_is_pps;
+  wire            ext_ref_locked;
+  // assignments
+  assign gp_out[85:0] = gp_out_s[85:0];
+  assign gp_in_s[95:86] = gp_out_s[95:86];
+  assign gp_in_s[85: 0] = gp_in[85:0];
+
+  // instantiations
+
+  // board gpio - 31-0
+
+  assign gpio_i[31:17] = gpio_o[31:17];
+  assign gpio_i[13:0] = gpio_o[13:0];
+
+  // ad9361 gpio - 63-32
+
+  assign gpio_i[63:47] = gpio_o[63:47];
+  assign tx_amp_en = 1'b1;
+  assign eth_rst_n = 1'b1;
+
+  ad_iobuf #(.DATA_WIDTH(15)) i_iobuf (
+    .dio_t ( gpio_t[46:32]),
+    .dio_i ( gpio_o[46:32]),
+    .dio_o ( gpio_i[46:32]),
+    .dio_p ({ gpio_resetb,        // 46:46
+              gpio_sync,          // 45:45
+              gpio_en_agc,        // 44:44
+              gpio_ctl,           // 43:40
+              gpio_status}));     // 39:32
+
+  // assign gpio_i[14] = ext_ref_locked;
+  // assign ext_ref_is_pps = gpio_o[15];
+  // assign ref_sel = gpio_o[16];
+  // assign tx_amp_en1 = 1'b1;
+  // assign tx_amp_en2 = 1'b1;
+
+  assign eth_rst_n = 1'b1;
+
+  gen_clks gen_clks(
+      .clk_out1(int_40mhz),       // output clk_out1
+      .clk_out2(),                // output clk_out2
+      .clk_out3(ref_pll_clk),     // output clk_out3
+      .locked(locked),            // output locked
+
+      .clk_in1(CLK_40MHz_FPGA)
+  );
+
+  assign ext_ref = ext_ref_is_pps ? PPS_IN : ref_sel ? CLKIN_10MHz : 1'b0;
+  b205_ref_pll ref_pll(
+      .reset(~locked),
+      .clk(ref_pll_clk),
+      .refclk(int_40mhz),
+      .ref_x(ext_ref),
+      .locked(ext_ref_locked),
+      .sclk(CLK_40M_DAC_SCLK),
+      .mosi(CLK_40M_DAC_DIN),
+      .sync_n(CLK_40M_DAC_nSYNC)
+  );
+
+
+  vio_0 u_vio (
+    .clk(int_40mhz),                  // input wire clk
+    .probe_in0(ext_ref_locked),      // input wire [0 : 0] probe_in0
+    .probe_out0(ext_ref_is_pps),    // output wire [0 : 0] probe_out0
+    .probe_out1(ref_sel),    // output wire [0 : 0] probe_out1
+    .probe_out2(FE_TXRX2_SEL2),    // output wire [0 : 0] probe_out2
+    .probe_out3(FE_TXRX2_SEL1),    // output wire [0 : 0] probe_out3
+    .probe_out4(FE_TXRX1_SEL2),    // output wire [0 : 0] probe_out4
+    .probe_out5(FE_TXRX1_SEL1),    // output wire [0 : 0] probe_out5
+    .probe_out6(FE_RX2_SEL2),    // output wire [0 : 0] probe_out6
+    .probe_out7(FE_RX2_SEL1),    // output wire [0 : 0] probe_out7
+    .probe_out8(FE_RX1_SEL2),    // output wire [0 : 0] probe_out8
+    .probe_out9(FE_RX1_SEL1),    // output wire [0 : 0] probe_out9
+    .probe_out10(tx_amp_en1),  // output wire [0 : 0] probe_out10
+    .probe_out11(tx_amp_en2)  // output wire [0 : 0] probe_out11
+  );
+  // instantiations
+
+  system_wrapper i_system_wrapper (
+    .MDIO_PHY_mdc(MDIO_PHY_mdc),
+    .MDIO_PHY_mdio_io(MDIO_PHY_mdio_io),
+    .RGMII_rd(RGMII_rd),
+    .RGMII_rx_ctl(RGMII_rx_ctl),
+    .RGMII_rxc(RGMII_rxc),
+    .RGMII_td(RGMII_td),
+    .RGMII_tx_ctl(RGMII_tx_ctl),
+    .RGMII_txc(RGMII_txc),
+    .ddr_addr (ddr_addr),
+    .ddr_ba (ddr_ba),
+    .ddr_cas_n (ddr_cas_n),
+    .ddr_ck_n (ddr_ck_n),
+    .ddr_ck_p (ddr_ck_p),
+    .ddr_cke (ddr_cke),
+    .ddr_cs_n (ddr_cs_n),
+    .ddr_dm (ddr_dm),
+    .ddr_dq (ddr_dq),
+    .ddr_dqs_n (ddr_dqs_n),
+    .ddr_dqs_p (ddr_dqs_p),
+    .ddr_odt (ddr_odt),
+    .ddr_ras_n (ddr_ras_n),
+    .ddr_reset_n (ddr_reset_n),
+    .ddr_we_n (ddr_we_n),
+    .enable (enable),
+    .fixed_io_ddr_vrn (fixed_io_ddr_vrn),
+    .fixed_io_ddr_vrp (fixed_io_ddr_vrp),
+    .fixed_io_mio (fixed_io_mio),
+    .fixed_io_ps_clk (fixed_io_ps_clk),
+    .fixed_io_ps_porb (fixed_io_ps_porb),
+    .fixed_io_ps_srstb (fixed_io_ps_srstb),
+    .gp_in_0 (gp_in_s[31:0]),
+    .gp_in_1 (gp_in_s[63:32]),
+    .gp_in_2 (gp_in_s[95:64]),
+    .gp_in_3 (32'd0),
+    .gp_out_0 (gp_out_s[31:0]),
+    .gp_out_1 (gp_out_s[63:32]),
+    .gp_out_2 (gp_out_s[95:64]),
+    .gp_out_3 (),
+    .gpio_i (gpio_i),
+    .gpio_o (gpio_o),
+    .gpio_t (gpio_t),
+    .gps_pps (1'b0),
+    .otg_vbusoc (1'b0),
+    .rx_clk_in_n (rx_clk_in_n),
+    .rx_clk_in_p (rx_clk_in_p),
+    .rx_data_in_n (rx_data_in_n),
+    .rx_data_in_p (rx_data_in_p),
+    .rx_frame_in_n (rx_frame_in_n),
+    .rx_frame_in_p (rx_frame_in_p),
+    .spi0_clk_i (1'b0),
+    .spi0_clk_o (spi_clk),
+    .spi0_csn_0_o (spi_csn),
+    .spi0_csn_1_o (),
+    .spi0_csn_2_o (),
+    .spi0_csn_i (1'b1),
+    .spi0_sdi_i (spi_miso),
+    .spi0_sdo_i (1'b0),
+    .spi0_sdo_o (spi_mosi),
+    .spi1_clk_i (1'b0),
+    .spi1_clk_o (),
+    .spi1_csn_0_o (),
+    .spi1_csn_1_o (),
+    .spi1_csn_2_o (),
+    .spi1_csn_i (1'b1),
+    .spi1_sdi_i (1'b0),
+    .spi1_sdo_i (1'b0),
+    .spi1_sdo_o (),
+    .tdd_sync_i (1'b0),
+    .tdd_sync_o (),
+    .tdd_sync_t (),
+    .tx_clk_out_n (tx_clk_out_n),
+    .tx_clk_out_p (tx_clk_out_p),
+    .tx_data_out_n (tx_data_out_n),
+    .tx_data_out_p (tx_data_out_p),
+    .tx_frame_out_n (tx_frame_out_n),
+    .tx_frame_out_p (tx_frame_out_p),
+    .txnrx (txnrx),
+    .up_enable (gpio_o[47]),
+    .up_txnrx (gpio_o[48]));
+
+endmodule
+
+// ***************************************************************************
+// ***************************************************************************
